ffb4c84ffef3f463f86698c1b1459798126cb58c
Max Tumanov <georgecarlin770@gmail.com>
2021-10-30 22:53:05 +0300

    proper project structure

---
+++ src/fibb.rs
@@ -0,0 +1,32 @@
+pub fn fibb()
+{
+    loop
+    { 
+        println!("What nth fibb number?:");
+        let mut input = String::new();
+        std::io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line.");
+        let input: i32 = match input.trim().parse()
+        {
+            Ok(num) => num,
+            // if not an integer, skip and ask again
+            Err(_) => continue,
+        };
+        let ending = match input % 10
+        {
+            1 => "st",
+            2 => "nd",
+            3 => "rd",
+            _ => "th",
+        };
+        let answer: i32 = nthfibb(input);
+        println!("{}{} fibb number is {}", input, ending, answer);
+    }
+}
+
+fn nthfibb(n: i32) -> i32
+{
+    if n <= 1 {return n}
+    else {return nthfibb(n-1)+nthfibb(n-2)}
+}
--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,6 @@
+mod fibb;
+
 fn main()
 {
-    loop
-    { 
-        println!("What nth fibb number?:");
-        let mut input = String::new();
-        std::io::stdin()
-            .read_line(&mut input)
-            .expect("Failed to read line.");
-        let input: i32 = match input.trim().parse()
-        {
-            Ok(num) => num,
-            // if not an integer, skip and ask again
-            Err(_) => continue,
-        };
-        let ending = match input % 10
-        {
-            1 => "st",
-            2 => "nd",
-            3 => "rd",
-            _ => "th",
-        };
-        let answer: i32 = fibb(input);
-        println!("{}{} fibb number is {}", input, ending, answer);
-    }
-}
-fn fibb(n: i32) -> i32
-{
-    if n <= 1 {return n}
-    else {return fibb(n-1)+fibb(n-2)}
+    fibb::fibb();
 }
